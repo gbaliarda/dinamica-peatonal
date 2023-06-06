f38df65713a3b8fa6f8e2d003fae91674a4d2297
Ezequiel <eperez@itba.edu.ar>
2023-06-06 19:56:03 -0300

    feat: add slope comparison among exit widths

--- visualization/flow_rate.py
+++ visualization/flow_rate.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 
 def main() -> None:
     # Get cumulative exits per time, for different pedestrians' initial positions
-    simulations = run_simulations()
-
     with open("config.toml", "r") as f:
         config = toml.load(f)
 
+    simulations = run_simulations(config)
+
     exit_widths = config["benchmarks"]["exitWidths"]
+    pedestrians = config["benchmarks"]["pedestrians"]
+
+    # Plot cumulative exits per time, for each exit width
+    exit_rate_comp(simulations, exit_widths, pedestrians)
+
+    # Plot flow rate vs exit width
     flow_rates = []
     errors = []
 
@@ -51,15 +57,13 @@ def main() -> None:
     plt.show()
 
 
-def run_simulations(rounds: int = 3):
-    with open("config.toml", "r") as f:
-        config = toml.load(f)
-
+def run_simulations(config, rounds: int = 3):
     exit_width = config["benchmarks"]["exitWidths"]
     pedestrians = config["benchmarks"]["pedestrians"]
     
     # Average flow rate for each exit width
     simulations = {}
+    simulations["rounds"] = rounds
 
     for i, d in enumerate(exit_width):
         config["simulation"]["exitWidth"] = d
@@ -91,22 +95,26 @@ def run_simulations(rounds: int = 3):
                 data = line.split()
                 time = float(data[0])
                 cumulative_exits = int(data[1])
-                # Take only the times in the stationary period
-                if time >= 10 and time <= 45:
-                    simulations[d][j]["times"].append(time)
-                    simulations[d][j]["exits"].append(cumulative_exits)
+                simulations[d][j]["times"].append(time)
+                simulations[d][j]["exits"].append(cumulative_exits)
+            
+            # Get the stationary period, between 10 and 45 seconds
+            lower_bound = np.where(np.array(simulations[d][j]["times"]) >= 10)[0][0]
+            upper_bound = np.where(np.array(simulations[d][j]["times"]) <= 45)[0][-1]
+            stationary_times = np.array(simulations[d][j]["times"])[lower_bound:upper_bound]
+            stationary_exits = np.array(simulations[d][j]["exits"])[lower_bound:upper_bound]
 
             # Linear regression
-            coefficients = np.polyfit(simulations[d][j]["times"], simulations[d][j]["exits"], 1)
+            coefficients = np.polyfit(stationary_times, stationary_exits, 1)
             slope = coefficients[0]
             intercept = coefficients[1]
             # Calculate the curve of best fit
-            curve = slope * np.array(simulations[d][j]["times"]) + intercept
+            curve = slope * stationary_times + intercept
 
             # PLot 1 linear regression as an example
             if j == 0 and d == 2.4:
-                plt.scatter(simulations[d][j]["times"][::50], simulations[d][j]["exits"][::50], color='blue')
-                plt.plot(simulations[d][j]["times"][::50], curve[::50], color='red', label=f"y = {slope:.2f}t + {intercept:.2f}")
+                plt.scatter(stationary_times[::50], stationary_exits[::50], color='blue')
+                plt.plot(stationary_times[::50], curve[::50], color='red', label=f"y = {slope:.2f}t + {intercept:.2f}")
 
                 # Customize the plot
                 plt.xlabel("Tiempo (s)", fontsize=18)
@@ -126,6 +134,51 @@ def run_simulations(rounds: int = 3):
         simulations[d]["flow_rate"] = (np.mean(flow_rates), np.std(flow_rates))
 
     return simulations
+
+
+def exit_rate_comp(simulations, exit_widths, pedestrians):
+    # Plot cumulative exits per time
+    for i, d in enumerate(exit_widths):
+        max_exits = pedestrians[i]
+        exits_step = max_exits // 20
+        average_times = {}
+
+        for j in range(simulations["rounds"]):
+            times = np.array(simulations[d][j]["times"])
+            exits = np.array(simulations[d][j]["exits"])
+
+            for exit_count in range(0, max_exits + 1, exits_step):
+                # Find the first occurrance of the given step in the exits array
+                index = np.where(exits >= exit_count)[0][0] # the equality might not exist, so we take the first greater value
+                try:
+                    average_times[exit_count].append(times[index])
+                except KeyError:
+                    average_times[exit_count] = [times[index]]
+        
+        # Iterate over the `average_times` keys and values, and compute the average time for each exit count
+        exits = []
+        times = []
+        errors = []
+
+        for exit_count, _times in average_times.items():
+            average_time = np.mean(_times)
+            std = np.std(_times)
+            exits.append(exit_count)
+            times.append(average_time)
+            errors.append(std)
+        
+        # Plot exit count vs average time, with horizontal error bars
+        plt.errorbar(times, exits, xerr=errors, fmt='o', capsize=3, markersize=4, color="black")
+        plt.plot(times, exits, label=f"d={d} ; N={max_exits}")
+
+    plt.xlabel("Tiempo (s)", fontsize=18)
+    plt.ylabel("Egresos", fontsize=18)
+
+    plt.tight_layout()
+    plt.grid()
+    plt.legend()
+    plt.savefig("out/flow_rate_comp.png")
+    plt.show()
 
 
 if __name__ == '__main__':
